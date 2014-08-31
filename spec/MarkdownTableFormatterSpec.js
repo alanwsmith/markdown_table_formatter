612b29369c8fc3223d349831103c4d6dd2329a5f
Alan W. Smith <alan@alanwsmith.com>
2014-08-31 08:32:42 -0400

    Updated so instance variables are defined in the prototype (which would make them class variables

--- spec/MarkdownTableFormatterSpec.js
+++ spec/MarkdownTableFormatterSpec.js
@@ -12,16 +12,20 @@ describe("MarkdownTableFormatter", function() {
     expect(mtf.format_table(input_table)).toEqual("| h1 | h2 | h3 |\n|----|----|----|\n| d1 | d2 | d3 |");
   });
 
-  // it("should properly set the input_table when format_table() is called.", function() {
-
-  // });
-
   it("should properly identify target column widths when source has no padding", function() {
     input_table = "|h1|h2|h3|\n|-|-|-|\n|d1|d2|d3|";
     mtf.format_table(input_table);
     expect(mtf.column_widths[0]).toBe(2);
     expect(mtf.column_widths[1]).toBe(2);
     expect(mtf.column_widths[2]).toBe(2);
+  });
+
+  it("should properly identify a second set of target column widths when source has no padding", function() {
+    input_table = "|h1_b|h2_b|h3_b|\n|-|-|-|\n|d1_b|d2_b|d3_b|";
+    mtf.format_table(input_table);
+    expect(mtf.column_widths[0]).toBe(4);
+    expect(mtf.column_widths[1]).toBe(4);
+    expect(mtf.column_widths[2]).toBe(4);
   });
 
 });
--- src/MarkdownTableFormatter.js
+++ src/MarkdownTableFormatter.js
@@ -1,19 +1,40 @@
 function MarkdownTableFormatter() {
 }
 
-// Initialize instance variables
-MarkdownTableFormatter.prototype.column_widths = new Array();
-MarkdownTableFormatter.prototype.input_table = "";
-
-
 MarkdownTableFormatter.prototype.get_column_widths = function() {
-  this.column_widths[0] = 2;
-  this.column_widths[1] = 2;
-  this.column_widths[2] = 2;
+
+  this.column_widths = new Array();  
+  this.input_lines = this.input_table.split("\n");
+
+  for (var lines_i = 0, lines_l = this.input_lines.length; lines_i < lines_l; lines_i = lines_i + 1) {
+    
+    var cols = this.input_lines[lines_i].split("\|");
+
+    // remove the first and last that are just spacers for the markdown borders
+    cols.shift();
+    cols.pop();
+  
+    // Loop through the items in each column
+    for (var cols_i = 0, cols_l = cols.length; cols_i < cols_l; cols_i = cols_i + 1) {
+
+      console.log("String: " + cols[cols_i] + " Length: " + cols[cols_i].length + " Was: " + this.column_widths[cols_i]);
+      if (typeof this.column_widths[cols_i] === 'undefined') {
+        this.column_widths[cols_i] = cols[cols_i].length;
+      }
+
+      if (this.column_widths[cols_i] < cols[cols_i].length) {
+        this.column_widths[cols_i] = cols[cols_i].length;
+      }
+    }
+  }  
+
 };
 
 MarkdownTableFormatter.prototype.format_table = function(input_table) {
+  console.log("Starting MarkdownTableFormatter.format_table()");
+  this.input_table = input_table;
   this.get_column_widths();
+  console.log("Finishing MarkdownTableFormatter.format_table()");
   return input_table;
 };
 
