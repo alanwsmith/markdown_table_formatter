41f95cb1fe1c9a91b2abe26c98119850503203a4
Alan W. Smith <alan@alanwsmith.com>
2014-08-31 05:51:20 -0400

    Refactored tests to use an 'input_table' that is cleared out before each use.

--- spec/MarkdownTableFormatterSpec.js
+++ spec/MarkdownTableFormatterSpec.js
@@ -1,19 +1,24 @@
 describe("MarkdownTableFormatter", function() {
   var mtf;
+  var input_table;
 
   beforeEach(function() {
     mtf = new MarkdownTableFormatter();
+    input_table = "";
   });
 
   it("should not alter an already formatted table", function() {
-    var formatted_table = "| h1 | h2 | h3 |\n|----|----|----|\n| d1 | d2 | d3 |";
-    expect(mtf.format_table(formatted_table)).toEqual(formatted_table);
+    input_table = "| h1 | h2 | h3 |\n|----|----|----|\n| d1 | d2 | d3 |";
+    expect(mtf.format_table(input_table)).toEqual("| h1 | h2 | h3 |\n|----|----|----|\n| d1 | d2 | d3 |");
   });
 
-  it("should properly identify target column widths when source has no padding", function() {
-    var formatted_table = "|h1|h2|h3|\n|-|-|-|\n|d1|d2|d3|";
+  // it("should properly set the input_table when format_table() is called.", function() {
 
-    mtf.format_table(formatted_table);
+  // });
+
+  it("should properly identify target column widths when source has no padding", function() {
+    input_table = "|h1|h2|h3|\n|-|-|-|\n|d1|d2|d3|";
+    mtf.format_table(input_table);
     expect(mtf.column_widths[0]).toBe(2);
     expect(mtf.column_widths[1]).toBe(2);
     expect(mtf.column_widths[2]).toBe(2);
--- src/MarkdownTableFormatter.js
+++ src/MarkdownTableFormatter.js
@@ -3,6 +3,7 @@ function MarkdownTableFormatter() {
 
 // Initialize instance variables
 MarkdownTableFormatter.prototype.column_widths = new Array();
+MarkdownTableFormatter.prototype.input_table = "";
 
 
 MarkdownTableFormatter.prototype.get_column_widths = function() {
