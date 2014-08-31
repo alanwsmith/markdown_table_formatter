76e4f787c2a2b6f97a9e4fa602ed7773c7881155
Alan W. Smith <alan@alanwsmith.com>
2014-08-31 05:45:16 -0400

    Stubbing out the get_column_widths method.

--- spec/MarkdownTableFormatterSpec.js
+++ spec/MarkdownTableFormatterSpec.js
@@ -10,4 +10,13 @@ describe("MarkdownTableFormatter", function() {
     expect(mtf.format_table(formatted_table)).toEqual(formatted_table);
   });
 
+  it("should properly identify target column widths when source has no padding", function() {
+    var formatted_table = "|h1|h2|h3|\n|-|-|-|\n|d1|d2|d3|";
+
+    mtf.format_table(formatted_table);
+    expect(mtf.column_widths[0]).toBe(2);
+    expect(mtf.column_widths[1]).toBe(2);
+    expect(mtf.column_widths[2]).toBe(2);
+  });
+
 });
--- src/MarkdownTableFormatter.js
+++ src/MarkdownTableFormatter.js
@@ -1,7 +1,18 @@
 function MarkdownTableFormatter() {
 }
 
+// Initialize instance variables
+MarkdownTableFormatter.prototype.column_widths = new Array();
+
+
+MarkdownTableFormatter.prototype.get_column_widths = function() {
+  this.column_widths[0] = 2;
+  this.column_widths[1] = 2;
+  this.column_widths[2] = 2;
+};
+
 MarkdownTableFormatter.prototype.format_table = function(input_table) {
+  this.get_column_widths();
   return input_table;
 };
 
