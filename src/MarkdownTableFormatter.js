2f9ff03dd4e3f43b1ffecb1fe96095eb75f4f7f0
Alan W. Smith <alan@alanwsmith.com>
2014-08-31 11:53:58 -0400

    Refactored out 'set_intput_lines' and 'get_column_widths' with 'set_input_cells' and 'set_column_widths'.

--- src/MarkdownTableFormatter.js
+++ src/MarkdownTableFormatter.js
@@ -6,34 +6,12 @@ function MarkdownTableFormatter() {
 MarkdownTableFormatter.prototype.format_table = function(input_table) {
   console.log("Starting MarkdownTableFormatter.format_table()");
   this.input_table = input_table;
-  this.set_intput_lines(input_table);
-  this.get_column_widths();
+  this.set_input_cells(input_table);
+  this.set_column_widths(this.input_cells);
   console.log("Finishing MarkdownTableFormatter.format_table()");
   return input_table;
 };
 
-
-////////////////////////////////////////////////////////////////////////////////
-
-MarkdownTableFormatter.prototype.get_column_widths = function() {
-
-  this.column_widths = new Array();  
-  
-  for (var lines_i = 0, lines_l = this.input_lines.length; lines_i < lines_l; lines_i = lines_i + 1) {
-    
-    var cols = this.input_lines[lines_i].split("\|");
-
-    // Loop through the items in each column
-    for (var cols_i = 0, cols_l = cols.length; cols_i < cols_l; cols_i = cols_i + 1) {
-      if (typeof this.column_widths[cols_i] === 'undefined') {
-        this.column_widths[cols_i] = cols[cols_i].length;
-      }
-      else if (this.column_widths[cols_i] < cols[cols_i].length) {
-        this.column_widths[cols_i] = cols[cols_i].length;
-      }
-    }
-  }
-};
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -72,14 +50,3 @@ MarkdownTableFormatter.prototype.set_input_cells = function(input_table) {
     }
   }
 }
-
-
-////////////////////////////////////////////////////////////////////////////////
-
-MarkdownTableFormatter.prototype.set_intput_lines = function(input_table) {
-
-  this.input_lines = input_table.split("\n");
-
-}
-
-
