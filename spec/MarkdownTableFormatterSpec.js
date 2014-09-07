2ca23d68c053903fd1339e4fe658b52bdf175f06
Alan W. Smith <alan@alanwsmith.com>
2014-09-07 16:34:27 -0400

    Updated test order to keep them alphabetical

--- spec/MarkdownTableFormatterSpec.js
+++ spec/MarkdownTableFormatterSpec.js
@@ -8,33 +8,6 @@ describe("MarkdownTableFormatter", function() {
 
   });
 
-
-  ////////////////////////////////////////////////////////////////////////////////
-  describe("import_table", function() {
-
-    it("should import a basic table properly", function() {
-
-      // WHEN
-      mtf.import_table('|h1|h2|\n|-|-|\n|d1|d2|');
-
-      // THEN
-      expect(mtf.cells).toEqual([ ['h1', 'h2'], ['-','-'], ['d1','d2'] ]);
-
-    });
-
-    it("should strip external white space when importing table", function() {
-
-      // WHEN
-      mtf.import_table('| h1 |h2 |  h3|\n|-|-|-|\n|   d1|d2   |       d3     |');
-
-      // THEN
-      expect(mtf.cells).toEqual([ ['h1', 'h2', 'h3'], ['-','-', '-'], ['d1','d2','d3'] ]);
-
-    });
-
-  });
-
-
   ////////////////////////////////////////////////////////////////////////////////
 
   describe("add_missing_cell_columns", function() {
@@ -62,6 +35,32 @@ describe("MarkdownTableFormatter", function() {
     });
   });
 
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+  describe("import_table", function() {
+
+    it("should import a basic table properly", function() {
+
+      // WHEN
+      mtf.import_table('|h1|h2|\n|-|-|\n|d1|d2|');
+
+      // THEN
+      expect(mtf.cells).toEqual([ ['h1', 'h2'], ['-','-'], ['d1','d2'] ]);
+
+    });
+
+    it("should strip external white space when importing table", function() {
+
+      // WHEN
+      mtf.import_table('| h1 |h2 |  h3|\n|-|-|-|\n|   d1|d2   |       d3     |');
+
+      // THEN
+      expect(mtf.cells).toEqual([ ['h1', 'h2', 'h3'], ['-','-', '-'], ['d1','d2','d3'] ]);
+
+    });
+
+  });
 
 
   ////////////////////////////////////////////////////////////////////////////////
