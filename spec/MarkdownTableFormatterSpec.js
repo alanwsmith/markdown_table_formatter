cbf95835aa2f97ecd5f0975053e152bde800dd19
Alan W. Smith <alan@alanwsmith.com>
2014-09-07 13:58:11 -0400

    Importing basic cells in the new version of MarkdownTableFormatter.

--- spec/MarkdownTableFormatterSpec.js
+++ spec/MarkdownTableFormatterSpec.js
@@ -1,10 +1,33 @@
 describe("MarkdownTableFormatter", function() {
 
+  // Initialize variables
   var mtf;
 
   beforeEach(function() {    
     mtf = new MarkdownTableFormatter();
+
   });
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+  describe("import_table", function() {
+
+    it("should import a basic table properly", function() {
+
+      // WHEN
+      mtf.import_table('|h1|h2|\n|-|-|\n|d1|d2|');
+
+      // THEN
+      expect(mtf.cells).toEqual([ ['h1', 'h2'], ['-','-'], ['d1','d2'] ]);
+
+
+
+    });
+  });
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////
 
   describe("initialization", function() {
 
@@ -18,7 +41,6 @@ describe("MarkdownTableFormatter", function() {
 
     });
   });
-
 
 
 
--- src/MarkdownTableFormatter.js
+++ src/MarkdownTableFormatter.js
@@ -2,7 +2,30 @@ function MarkdownTableFormatter() {
 
   // Setup instance variables.
   this.cells = new Array();
-  
 
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+MarkdownTableFormatter.prototype.import_table = function(table) {
+  
+  var table_rows = table.split("\n");
+
+  for (var row_i = 0, row_l = table_rows.length; row_i < row_l; row_i = row_i + 1) {
+
+    this.cells[row_i] = new Array();
+
+    var row_columns = table_rows[row_i].split("\|");
+
+    // TODO: Update so removal of first and last empty columns
+    // is only done with "|data|data|" rows and note for
+    // "data|data" rows without leading and ending pipes.
+    row_columns.shift();
+    row_columns.pop();
+
+    for (var col_i = 0, col_l = row_columns.length; col_i < col_l; col_i = col_i + 1) {
+      this.cells[row_i][col_i] = row_columns[col_i]
+      
+    }
+  }
+}
